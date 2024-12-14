c38feb3d5afcc9a0bc54e3a870812ecf17fb4380
ns-morozova <ns_morozova@mail.ru>
2024-12-14 10:39:37 +0300

    fixed

--- 003-magic01/index.php
+++ 003-magic01/index.php
@@ -8,12 +8,27 @@ include 'PeopleList.php';
 $person1 = new Person('Иван', 'Иванов', 'ivanov', 'password123');
 $person2 = new Person('Петр', 'Петров', 'petrov', 'password456');
 
+// Сериализация объекта
+$serializedPerson = serialize($person1);
+echo "Сериализованный объект: " . $serializedPerson . PHP_EOL;
+
+// Изменение строки (например, изменение логина)
+$modifiedSerializedPerson = str_replace('ivanov', 'petrov', $serializedPerson);
+echo "Измененная сериализованная строка: " . $modifiedSerializedPerson . PHP_EOL;
+
+// Десериализация объекта
+$deserializedPerson = unserialize($modifiedSerializedPerson);
+echo "Десериализованный объект: " . $deserializedPerson . PHP_EOL;
+
+
+
 // Создание списка людей
 $peopleList = new PeopleList();
 $peopleList->addPerson($person1);
 $peopleList->addPerson($person2);
 
 // Вывод списка людей через foreach
+echo "Вывод списка людей:" . PHP_EOL;
 foreach ($peopleList as $person) {
     echo $person . PHP_EOL;
 }
